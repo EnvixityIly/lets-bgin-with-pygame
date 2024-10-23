f897c5e47c96e6eb4d12d448af613fb1e0a4e454
Zabir Hasan <dominicancongo@gmail.com>
2024-10-23 22:02:32 +0600

    a

---
+++ acp.py
@@ -0,0 +1,26 @@
+import pygame
+
+pygame.init()
+white = (255,255,255)
+
+clock = pygame.time.Clock()
+display_surface = pygame.display.set_mode((500,500))
+
+pygame.display.set_caption("jjk")
+
+DEFAULT_IMAGE_SIZE = (200,200)
+DEFAULT_IMAGE_POS = (150,150)
+
+image = pygame.image.load('th.jpeg')
+image = pygame.transform.scale(image,DEFAULT_IMAGE_SIZE)
+
+while True:
+    display_surface.fill(white)
+    display_surface.blit(image,DEFAULT_IMAGE_POS)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit( )
+            quit()
+    pygame.display.flip()
+    clock.tick(90)
